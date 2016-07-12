c1dd43bac97f3d333cb437747c7f4a6d9fb81a7a
Justin <Crank50@icloud.com>
2016-07-12 15:31:02 -0600

    Alien Game

--- src/Game/PlayNVA.java
+++ src/Game/PlayNVA.java
@@ -34,36 +34,65 @@ public class PlayNVA {
                 System.out.println("Invalid player type.");
             }
         }
-        String weapon = "";
-        while (!((playerType.equalsIgnoreCase("ninja") && ((weapon.equalsIgnoreCase("blade")) || (weapon.equalsIgnoreCase("star")))) ||
-                ((playerType.equalsIgnoreCase("alien") && ((weapon.equalsIgnoreCase("claw")) || (weapon.equalsIgnoreCase("laser"))))))) {
+        String weaponType = "";
+        while (!((playerType.equalsIgnoreCase("ninja") && ((weaponType.equalsIgnoreCase("blade")) || (weaponType.equalsIgnoreCase("star")))) ||
+                ((playerType.equalsIgnoreCase("alien") && ((weaponType.equalsIgnoreCase("claw")) || (weaponType.equalsIgnoreCase("laser"))))))) {
             if (playerType.equalsIgnoreCase("ninja")) {
                 System.out.println("Okay ninja, choose your weapon [blade/star]");
             } else {
                 System.out.println("Okay alien, choose your weapon [claw/laser]");
             }
-            weapon = scanner.nextLine();
-            if (playerType.equalsIgnoreCase("ninja") && weapon.equalsIgnoreCase("blade")) {
+            weaponType = scanner.nextLine();
+            if (playerType.equalsIgnoreCase("ninja") && weaponType.equalsIgnoreCase("blade")) {
                 System.out.println("The ninja blade is close range, but deals more damage");
-            } else if (playerType.equalsIgnoreCase("ninja") && weapon.equalsIgnoreCase("star")) {
+            } else if (weaponType.equalsIgnoreCase("ninja") && weaponType.equalsIgnoreCase("star")) {
                 System.out.println("The ninja star does less damage, but you can snipe from way the fuck back there");
-            } else if (playerType.equalsIgnoreCase("alien") && weapon.equalsIgnoreCase("claw")) {
+            } else if (weaponType.equalsIgnoreCase("alien") && weaponType.equalsIgnoreCase("claw")) {
                 System.out.println("The alien claw is close range but really fucks shit up");
-            } else if (playerType.equalsIgnoreCase("alien") && weapon.equalsIgnoreCase("laser")) {
+            } else if (weaponType.equalsIgnoreCase("alien") && weaponType.equalsIgnoreCase("laser")) {
                 System.out.println("The alien laser is GG");
             } else {
                 System.out.println("choose again dipshit.");
             }
-            System.out.println("Choose your location? [forest/space]");
-            String location = scanner.nextLine();
+        }
 
-            System.out.println("Choose your special attack? [speed/power/pain");
-            String special = scanner.nextLine();
+
+        String locationType = "";
+        while (!((locationType.equalsIgnoreCase("forest") || locationType.equalsIgnoreCase("space")))) {
+            System.out.println("Choose your location? [forest/space]");
+
+            locationType = scanner.nextLine();
+            if (locationType.equalsIgnoreCase("space")) {
+                System.out.println("Space gives the alien a huge strength advantage.");
+            } else if (locationType.equalsIgnoreCase("forest")) {
+                System.out.println("The forest gives the ninja a slight stealth advantage");
+            } else {
+                System.out.println("choose again dipshit");
+            }
+        }
+
+
+        String specialType = "";
+        while (!((specialType.equalsIgnoreCase("speed") || specialType.equalsIgnoreCase("power") || specialType.equalsIgnoreCase("pain")))) {
+            System.out.println("Choose your special attack? [speed/power/pain]");
+
+            specialType = scanner.nextLine();
+            if (specialType.equalsIgnoreCase("speed")) {
+                System.out.println("Speed gives the player a high chance of making a second hit at 20% damage of the first hit");
+            } else if (specialType.equalsIgnoreCase("power")) {
+                System.out.println("Power gives the player a medium chance of doing an additional 50% damage on a hit.");
+            } else if (specialType.equalsIgnoreCase("pain")) {
+                System.out.println("Pain has a small change of inflicting a permanent penalty on the opponent, decreasing their overall effectiveness by 10%.");
+            } else {
+                System.out.println("choose again dipshit");
+            }
 
 
         }
     }
 }
+
+
 
 
 
